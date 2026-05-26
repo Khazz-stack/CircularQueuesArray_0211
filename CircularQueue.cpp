685486c1a4ccd20619ef0a7dfcd3f76f7ace789d
Khazz <thelenaid@gmail.com>
2026-05-26 10:50:38 +0700

    jika FRONT <= REAR, terasi dari FRONT hingga akhir array

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -87,5 +87,13 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] <<" ";
+                FRONT_Position++;
+            }
+        }
     }
 };
