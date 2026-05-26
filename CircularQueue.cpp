b358c2454470b58d70d3e35a78df7f9c8a55aa01
Khazz <thelenaid@gmail.com>
2026-05-26 10:39:47 +0700

    cek jika antrian hanya memilki satu element

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -50,5 +50,11 @@ public:
         cout << "Queue underflow\n";
         return;
        } 
+       cout << "\nThe element deleted from the queue is: " <<queue_array[FRONT] << "\n";
+       if (FRONT == REAR)
+       {
+        FRONT = -1;
+        REAR = -1;
+       }
     }
 };
