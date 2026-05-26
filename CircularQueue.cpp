75f33768104859c8f6679a1e31ee1f06b2e992be
Khazz <thelenaid@gmail.com>
2026-05-26 10:44:55 +0700

    cek apakah antrian kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -69,5 +69,11 @@ public:
     {
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
+
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
     }
 };
